a10337511f84fd84bb5b690d376b116f9badfa6f
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-10 15:22:54 +0600

    firebase setup done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,87 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { AuthContext } from "../../Context/ContextProvider";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { updateProfile } from "firebase/auth";
 
 
 const Register = () => {
+
+
+    const { registerUser, googleUser, githubUser } = useContext(AuthContext)
+    const [error, setError] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+    const navigate = useNavigate();
+    const location = useLocation();
+
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+
+        // setError("")
+
+        const email = data.email
+        const password = data.password
+        const userName = data.userName
+        const photoUrl = data.photoUrl
+
+        const regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+        if (!regex.test(data.password)) {
+            setError("Password should be one Upper case, one lower case, and at least 6 characters")
+            return;
+        }
+
+        registerUser(email, password)
+            .then((result) => {
+                toast.success("Successfully register !")
+                navigate(location?.state ? location.state : '/login')
+                updateProfile(result.user, {
+                    userName: userName,
+                    photoURL: photoUrl,
+                    email: email,
+                })
+                    .then()
+                    .catch()
+            })
+            .catch()
+    }
+
+    const handleGoogleRegister = () => {
+
+        googleUser()
+            .then(() => {
+                toast.success("Successfully google register !")
+                navigate(location?.state ? location.state : '/login')
+            })
+            .catch(() => {
+                toast.warn("Email already exist")
+            })
+    }
+
+
+    const handleGiHubRegister = () => {
+
+        githubUser()
+            .then(() => {
+                toast.success("Successfully github register !")
+                navigate(location?.state ? location.state : '/login')
+            })
+            .catch(() => {
+                toast.warn("Email already exist")
+            })
+    }
+
+
     return (
-        <div className="mt-10 container mx-auto">
+        <div className="my-10 container mx-auto">
             <div className="flex h-screen">
 
                 <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
@@ -85,7 +163,7 @@ const Register = () => {
                         <h1 className="text-sm font-semibold mb-6 text-gray-500 text-center">Join to Our Community with all time access and free </h1>
                         <div className="mt-4 flex flex-col lg:flex-row items-center justify-between">
                             <div className="w-full lg:w-1/2 mb-2 lg:mb-0">
-                                <button type="button" className="w-full flex justify-center items-center gap-2 bg-white text-sm text-gray-600 p-2 rounded-md hover:bg-gray-50 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition-colors duration-300">
+                                <button onClick={handleGoogleRegister} type="button" className="w-full flex justify-center items-center gap-2 bg-white text-sm text-gray-600 p-2 rounded-md hover:bg-gray-50 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition-colors duration-300">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4" id="google">
                                         <path fill="#fbbb00" d="M113.47 309.408 95.648 375.94l-65.139 1.378C11.042 341.211 0 299.9 0 256c0-42.451 10.324-82.483 28.624-117.732h.014L86.63 148.9l25.404 57.644c-5.317 15.501-8.215 32.141-8.215 49.456.002 18.792 3.406 36.797 9.651 53.408z"></path>
                                         <path fill="#518ef8" d="M507.527 208.176C510.467 223.662 512 239.655 512 256c0 18.328-1.927 36.206-5.598 53.451-12.462 58.683-45.025 109.925-90.134 146.187l-.014-.014-73.044-3.727-10.338-64.535c29.932-17.554 53.324-45.025 65.646-77.911h-136.89V208.176h245.899z"></path>
@@ -93,7 +171,7 @@ const Register = () => {
                                         <path fill="#f14336" d="m419.404 58.936-82.933 67.896C313.136 112.246 285.552 103.82 256 103.82c-66.729 0-123.429 42.957-143.965 102.724l-83.397-68.276h-.014C71.23 56.123 157.06 0 256 0c62.115 0 119.068 22.126 163.404 58.936z"></path>
                                     </svg> Sign Up with Google </button>
                             </div>
-                            <div className="w-full lg:w-1/2 ml-0 lg:ml-2">
+                            <div onClick={handleGiHubRegister} className="w-full lg:w-1/2 ml-0 lg:ml-2">
                                 <button type="button" className="w-full flex justify-center items-center gap-2 bg-white text-sm text-gray-600 p-2 rounded-md hover:bg-gray-50 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition-colors duration-300">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="github" className="w-4">
                                         <path d="M7.999 0C3.582 0 0 3.596 0 8.032a8.031 8.031 0 0 0 5.472 7.621c.4.074.546-.174.546-.387 0-.191-.007-.696-.011-1.366-2.225.485-2.695-1.077-2.695-1.077-.363-.928-.888-1.175-.888-1.175-.727-.498.054-.488.054-.488.803.057 1.225.828 1.225.828.714 1.227 1.873.873 2.329.667.072-.519.279-.873.508-1.074-1.776-.203-3.644-.892-3.644-3.969 0-.877.312-1.594.824-2.156-.083-.203-.357-1.02.078-2.125 0 0 .672-.216 2.2.823a7.633 7.633 0 0 1 2.003-.27 7.65 7.65 0 0 1 2.003.271c1.527-1.039 2.198-.823 2.198-.823.436 1.106.162 1.922.08 2.125.513.562.822 1.279.822 2.156 0 3.085-1.87 3.764-3.652 3.963.287.248.543.738.543 1.487 0 1.074-.01 1.94-.01 2.203 0 .215.144.465.55.386A8.032 8.032 0 0 0 16 8.032C16 3.596 12.418 0 7.999 0z"></path>
@@ -103,22 +181,42 @@ const Register = () => {
                         <div className="mt-4 text-sm text-gray-600 text-center">
                             <p>or with email</p>
                         </div>
-                        <form className="space-y-4">
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
 
                             <div>
                                 <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
-                                <input type="text" id="username" name="username" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" />
+                                <input type="text" id="username" name="username" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" {...register("userName", { required: true })} />
+                                {errors.userName && <small className="text-red-500 font-medium mt-1">This field is required</small>}
                             </div>
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                                <input type="text" id="email" name="email" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" />
+                                <input type="text" id="email" name="email" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" {...register("email", { required: true })} />
+                                {errors.userName && <small className="text-red-500 font-medium mt-1">This field is required</small>}
                             </div>
-                            <div>
+                            <div className="relative">
                                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                                <input type="password" id="password" name="password" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" />
+                                <input type={showPassword ? "text" : "password"} id="password" name="password" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" {...register("password", {
+                                    required: true,
+                                })} />
+                                <span onClick={() => setShowPassword(!showPassword)} className="absolute text-xl top-9 right-6">
+                                    {
+                                        showPassword ? <FaRegEyeSlash /> : <FaRegEye />
+                                    }
+                                </span>
+                                {errors.password && <small className="text-red-500 font-medium mt-1">This field is required</small>}
+                                <div>
+                                    {
+                                        error && <small className="text-red-500 font-medium mt-1">{error}</small>
+                                    }
+                                </div>
                             </div>
                             <div>
-                                <button type="submit" className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign Up</button>
+                                <label htmlFor="photo" className="block text-sm font-medium text-gray-700">Photo Url</label>
+                                <input type="url" id="photo" name="photo" className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300" {...register("photoUrl", { required: true })} />
+                                {errors.userName && <small className="text-red-500 font-medium mt-1">This field is required</small>}
+                            </div>
+                            <div>
+                                <button type="submit" className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Register</button>
                             </div>
                         </form>
                         <div className="mt-4 text-sm text-gray-600 text-center">
