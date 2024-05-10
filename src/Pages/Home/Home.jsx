5a34717ea257ca2be62df87f530a68eeabda466b
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-10 20:54:36 +0600

    a tinny banner done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Slider from "../../Components/Slider/Slider";
+import TinnyBanner from "../../Components/TinnyBanner/TinnyBanner";
 
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <TinnyBanner></TinnyBanner>
         </div>
     );
 };
