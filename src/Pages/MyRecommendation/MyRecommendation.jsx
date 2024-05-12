612eaf6fa8f32b86b26cb0d259e0cf581377bee1
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-13 00:20:20 +0600

    added delete function for recommend

--- src/Pages/MyRecommendation/MyRecommendation.jsx
+++ src/Pages/MyRecommendation/MyRecommendation.jsx
@@ -1,24 +1,52 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context/ContextProvider";
+import TBody from "./TBody";
 
 
 const MyRecommendation = () => {
 
     const { user } = useContext(AuthContext);
-    const [MyRecommendation, setMyReMyRecommendation] = useState([]);
+    const [MyRecommendation, setMyRecommendation] = useState([]);
 
 
     useEffect(() => {
         fetch(`http://localhost:5000/myRecommendation/${user?.email}`)
             .then(res => res.json())
             .then(data => {
-                setMyReMyRecommendation(data);
+                setMyRecommendation(data);
             });
     }, [user]);
 
     return (
         <div>
-
+            <div className="container mt-5 p-2 mx-auto sm:p-4 lg:min-h-[400px]">
+                <h2 className="mb-4 text-2xl font-bold leading-tight fira-sans">Recommendation By Me</h2>
+                <div className="overflow-x-auto">
+                    <table className="min-w-full text-xs">
+                        <colgroup>
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col />
+                            <col className="w-24" />
+                        </colgroup>
+                        <thead className="dark:bg-gray-300">
+                            <tr className="text-left">
+                                <th className="p-3">Product Name</th>
+                                <th className="p-3">Recommended Product Name</th>
+                                <th className="p-3">Recommendation Title</th>
+                                <th className="p-3">Date</th>
+                                <th className="p-3">Recommender Email</th>
+                                <th className="p-3 text-right"></th>
+                            </tr>
+                        </thead>
+                        {
+                            MyRecommendation.map(mr => <TBody setMyRecommendation={setMyRecommendation} MyRecommendation={MyRecommendation} key={mr._id} mr={mr}></TBody> )
+                        }
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/MyRecommendation/TBody.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/prop-types */
+import Swal from "sweetalert2";
+
+
+const TBody = ({ mr, setMyRecommendation, MyRecommendation }) => {
+
+    const { _id } = mr;
+
+
+    const handleDelete = id => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/myRecommendation/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Query has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = MyRecommendation.filter(mr => mr._id !== _id);
+                            setMyRecommendation(remaining);
+                        }
+                    })
+            }
+        })
+    }
+
+    return (
+        <tbody>
+            <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
+                <td className="p-3">
+                    <p>{mr?.productNameForRecommend}</p>
+                </td>
+                <td className="p-3">
+                    <p>{mr?.recommendedProductName}</p>
+                </td>
+                <td className="p-3">
+                    <p>{mr?.recommendationTitle}</p>
+                </td>
+                <td className="p-3">
+                    <p className="dark:text-gray-600">{mr?.date}</p>
+                </td>
+                <td className="p-3">
+                    <p className="dark:text-gray-600">{mr?.recommenderEmail}</p>
+                </td>
+                <td className="p-3 text-right">
+                    <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">
+                        <span>
+                            <button onClick={() => handleDelete(_id)}>Delete</button>
+                        </span>
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+    );
+};
+
+export default TBody;
