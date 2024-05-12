0fe67df69115df00c0ac67692f5d241da042505f
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-13 03:05:43 +0600

    dynamic grid layout done

--- src/Pages/AllQueries/AllQueries.jsx
+++ src/Pages/AllQueries/AllQueries.jsx
@@ -1,19 +1,33 @@
 import { useLoaderData } from "react-router-dom";
 import AllQueriesCard from "./AllQueriesCard";
+import { useState } from "react";
+import { BsLayoutSidebarReverse } from "react-icons/bs";
 
 
 const AllQueries = () => {
 
     const allQueries = useLoaderData()
-    console.log(allQueries);
+
+    const [isGrid, setIsGrid] = useState(true);
+
+    const toggleLayout = () => {
+        setIsGrid(!isGrid);
+    };
+
+
 
 
     return (
-        <div className="my-10">
+        <div className="my-10 container mx-auto">
 
             <h2 className="text-5xl font-bold text-center">Queries</h2>
 
-            <div className="grid grid-cols-1 lg:grid-cols-3 p-1 container mx-auto mt-10 gap-12">
+            <div>
+                <button onClick={toggleLayout} className="text-2xl mx-2"><BsLayoutSidebarReverse /></button>
+            </div>
+
+            <div className={`${isGrid ? 'grid lg:grid-cols-3 gap-12' : ''} grid grid-cols-1 p-1 container mx-auto mt-10 gap-12`}>
+
                 {
                     allQueries.map((data) => <AllQueriesCard data={data} key={data._id}></AllQueriesCard>)
                 }
